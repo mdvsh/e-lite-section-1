4371fbe51874f03a5ea32b15fe419bdea9087222
Madhav Sharma <shekharmadhav03@gmail.com>
2020-07-07 21:28:58 +0530

    site1:complete

---
+++ site1/data.js
@@ -0,0 +1,79 @@
+var data = [
+    {
+        'img': 'imgs/1.svg',
+        'type': 'Residential Apartment',
+        'addr': 'Tata Primanti, Sector 74, Gurgaon',
+        'price': 'mid',
+        'loc': 'gg'
+    },
+    {
+        'img': 'imgs/5.svg',
+        'type': 'Independent House',
+        'addr': 'Panscheel Marg, Chanakyapuri',
+        'price': 'high',
+        'loc': 'dl'
+    },
+    {
+        'img': 'imgs/2.svg',
+        'type': 'Residential Apartment',
+        'addr': 'DDA, Vasant Kunj, Delhi',
+        'price': 'low',
+        'loc': 'dl'
+    },
+    {
+        'img': 'imgs/3.svg',
+        'type': 'Residential Apartment',
+        'addr': 'Leela Sky Villas, Gurgaon',
+        'price': 'mid',
+        'loc': 'gg'
+    },
+    {
+        'img': 'imgs/4.svg',
+        'type': 'Office Space',
+        'addr': 'One Horizon, Golf Links, Gurgaon',
+        'price': 'high',
+        'loc': 'gg'
+    },
+    {
+        'img': 'imgs/3.svg',
+        'type': 'Land Plot',
+        'addr': 'Block-M, Greater Kailash',
+        'price': 'mid',
+        'loc': 'dl'
+    },
+    {
+        'img': 'imgs/2.svg',
+        'type': 'Office Space',
+        'addr': 'Inner Circle, Connaught Place',
+        'price': 'high',
+        'loc': 'dl'
+    },
+    {
+        'img': 'imgs/1.svg',
+        'type': 'Land Plot',
+        'addr': 'Chattarpur Farms, Delhi',
+        'price': 'low',
+        'loc': 'dl'
+    },
+    {
+        'img': 'imgs/4.svg',
+        'type': 'Independent House',
+        'addr': 'Vasant Vihar, Delhi',
+        'price': 'mid',
+        'loc': 'dl'
+    },
+    {
+        'img': 'https://www.todayifoundout.com/wp-content/uploads/2017/11/rick-astley.png',
+        'type': "Didn't see it coming ?",
+        'addr': "F",
+        'price': 'no price',
+        'loc': 'ot'
+    },
+    {
+        'img': 'https://i.redd.it/htgt2kkb5mf41.jpg',
+        'type': '',
+        'addr': "Near Nemo's Coral",
+        'price': 'no price',
+        'loc': 'ot'
+    }
+]
--- site1/script.js
+++ site1/script.js
@@ -5,4 +5,93 @@ $(document).ready(function () {
       $(".accordion_item").not($(this).closest(".accordion_item")).removeClass("open-acc");
       $(this).closest(".accordion_item").toggleClass("open-acc");
     });
-  });
+});
+prop_card = document.getElementById('prop-grid')
+function fill() {
+  prop_card.innerHTML = ''
+  for (let i = 0; i < data.length; i++) {
+    var property = data[i];
+    // console.log(property)
+    var prop_data;
+    prop_data = `
+      <div class="prop_card">
+        <div class="prop_image">
+        <img src="${property.img}" alt="a crib" />
+        </div>
+        <div class="prop_card_content">
+          <p class="prop_type">
+            ${property.type}
+          </p>
+          <div class="prop_addr">
+            <p>${property.addr}</p>
+            <span class="iconify" data-inline="false" data-icon="bi:bookmark" style="color: #5628d2; font-size: 25px; margin-left: auto;" onclick="javascript:alert('Property has been bookmarked.');" id="bb"></span>
+          </div>
+          <div class="more">MORE --></div>						
+        </div>
+      </div>
+    `
+    if (property.loc != 'ot') {
+      prop_card.innerHTML+=prop_data
+    }
+  };
+};
+function filter(fq, ft) {
+  // console.log(fq, ft)
+  prop_card.innerHTML = ''
+  for (let i = 0; i < data.length; i++) {
+    var property = data[i];
+    var prop_data;
+    prop_data = `
+      <div class="prop_card">
+        <div class="prop_image">
+        <img src="${property.img}" alt="a crib" />
+        </div>
+        <div class="prop_card_content">
+          <p class="prop_type">
+            ${property.type}
+          </p>
+          <div class="prop_addr">
+            <p>${property.addr}</p>
+            <span class="iconify" data-inline="false" data-icon="bi:bookmark" style="color: #5628d2; font-size: 25px; margin-left: auto;" onclick="javascript:alert('Property has been bookmarked.');" id="bb"></span>
+          </div>
+          <div class="more">MORE --></div>						
+        </div>
+      </div>
+    `
+    // console.log(property[ft] == fq)
+    if (property[ft] == fq) {
+      prop_card.innerHTML+=prop_data
+    }
+  }
+  // d = document.getElementById(ft)
+  // d.innerHTML+='<span class="iconify" data-inline="false" data-icon="ant-design:check-circle-filled" style="color: #5628d2; font-size: 22px;"></span>'
+};
+function search(sq) {
+  prop_card.innerHTML = ''
+  for (let i = 0; i < data.length; i++) {
+    var property = data[i];
+    var prop_data;
+    prop_data = `
+      <div class="prop_card">
+        <div class="prop_image">
+        <img src="${property.img}" alt="a crib" />
+        </div>
+        <div class="prop_card_content">
+          <p class="prop_type">
+            ${property.type}
+          </p>
+          <div class="prop_addr">
+            <p>${property.addr}</p>
+            <span class="iconify" data-inline="false" data-icon="bi:bookmark" style="color: #5628d2; font-size: 25px; margin-left: auto;" onclick="javascript:alert('Property has been bookmarked.');" id="bb"></span>
+          </div>
+          <div class="more">MORE --></div>						
+        </div>
+      </div>
+    `
+    if (((property.type.toLowerCase()).indexOf(sq.toLowerCase())) > -1) {
+      prop_card.innerHTML+=prop_data
+    }	
+
+  }		
+}
+
